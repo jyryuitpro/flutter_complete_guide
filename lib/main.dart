20a586e13ce1f3f2b50e7d0fca0abd3154b9d724
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-10-30 23:51:34 +0900

    Building an App From Scratch

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text(
+        'Hello!',
+      ),
+    );
+  }
+}
