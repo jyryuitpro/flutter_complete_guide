297d3ae4f64d7e96d917acef3c88b7996ebe2d00
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-10-31 15:30:56 +0900

    Updating Correctly with Stateful Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,21 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  var questionIndex = 0;
+class MyApp extends StatefulWidget {
 
   MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    questionIndex = questionIndex + 1;
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print(questionIndex);
   }
 
