3b49cc2cddbc4e087ce3984ed5728576c7c41b47
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-10-31 12:07:00 +0900

    Running the App on an Emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
