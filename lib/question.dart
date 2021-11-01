a862af3e3d26fa04e1124a8fc280988bdda2e751
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-11-01 14:16:33 +0900

    First Styling & Layouting Steps

--- lib/question.dart
+++ lib/question.dart
@@ -3,11 +3,20 @@ import 'package:flutter/material.dart';
 class Question extends StatelessWidget {
   final String questionText;
 
-
   Question(this.questionText, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
